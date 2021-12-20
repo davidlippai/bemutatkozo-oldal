b655975ccd66b62b305393af295193cac46f59ed
davidlippai <lippai28@gmail.com>
2021-12-20 19:24:46 +0100

    new style

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-
+/*Hamburger-Menu*/
 
 $("#hamburgerMenu").click(function(){
   $(".menu").slideToggle("600");
 })
+
+
+/*-------------Services--------*/
+
+
+/*$("#first-article").click(function(){
+  $("#first-paragraph").slideToggle("600");
+});
+
+$("#second-article").click(function(){
+  $("#second-paragraph").slideToggle("600");
+});
+
+$("#third-article").click(function(){
+  $("#third-paragraph").slideToggle("600");
+});*/
