f02f44fae0611ddd98699bb08db4a13857d36502
davidlippai <lippai28@gmail.com>
2021-12-20 19:36:46 +0100

    name writing

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ $("#hamburgerMenu").click(function(){
   $(".menu").slideToggle("600");
 })
 
+/*name typing*/
+
+let i = 0;
+let txt = 'David Lippai';
+let speed = 80;
+
+function typeWriter(){
+  if (i<txt.length) {
+    document.getElementById("myname").innerHTML +=txt.charAt(i);
+    i++;
+    setTimeout(typeWriter,speed);
+  }
+}
+
+window.onload = typeWriter;
 
 /*-------------Services--------*/
 
